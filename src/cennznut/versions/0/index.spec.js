6dc785ebde1d8c6d3b44619495d3046b6e3b9631
Eric <e.lim0322@gmail.com>
2019-10-02 12:55:49 +1300

    add 6 additional test cases
    - encodes 1 constraint as 0
    - encodes 2 constraints as 1
    - encodes 1 module with constraints
    - encodes 2 modules with constraints
    - decodes 0 constraint length byte as 1
    - decodes 1 constraint length byte as 2

--- src/cennznut/versions/0/index.spec.js
+++ src/cennznut/versions/0/index.spec.js
@@ -40,6 +40,46 @@ describe("Encode CENNZnut", () => {
       expect(encoded[0]).toEqual(0b10000000);
   });
 
+  it("encodes 1 constraint as 0", () => {
+    const encoded = cennznut.encode({
+      "modules": {
+        "generic-asset": {
+          "methods": {
+            "transfer": {
+              "constraints": [0]
+            }
+          }
+        }
+      }
+    });
+    const cursor = cennznut.MODULE_COUNT_BYTE_LENGTH +
+                   cennznut.MODULE_META_DATA_BYTE_LENGTH +
+                   cennznut.KEY_BYTE_LENGTH +
+                   cennznut.METHOD_META_DATA_BYTE_LENGTH +
+                   cennznut.KEY_BYTE_LENGTH;
+    expect(encoded[cursor]).toEqual(0b00000000);
+  });
+
+  it("encodes 2 constraints as 1", () => {
+    const encoded = cennznut.encode({
+      "modules": {
+        "generic-asset": {
+          "methods": {
+            "transfer": {
+              "constraints": [0, 1]
+            }
+          }
+        }
+      }
+    });
+    const cursor = cennznut.MODULE_COUNT_BYTE_LENGTH +
+                   cennznut.MODULE_META_DATA_BYTE_LENGTH +
+                   cennznut.KEY_BYTE_LENGTH +
+                   cennznut.METHOD_META_DATA_BYTE_LENGTH +
+                   cennznut.KEY_BYTE_LENGTH;
+    expect(encoded[cursor]).toEqual(0b10000000);
+  });
+
   it("encodes with valid block cooldown", () => {
     expect(() => {
       cennznut.encode({
@@ -73,12 +113,12 @@ describe("Encode CENNZnut", () => {
     );
   });
 
-  it("encodes with constraints", () => {
+  it("encodes 1 module with constraints", () => {
     const encodedTest = new Uint8Array([
       0, 64, 109, 111, 100, 117, 108, 101, 95, 116, 101, 115, 116, 0, 0, 0, 0,
       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 64, 109, 101, 116,
       104, 111, 100, 95, 116, 101, 115, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 148, 0, 192, 128, 16, 246, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20, 0, 192, 128, 16, 246, 0, 0, 0, 0, 0,
       0, 0, 128, 16, 178, 128, 0, 0, 0, 0, 0, 0, 0, 224, 116, 101, 115, 116,
       105, 110, 103, 5, 0, 0, 1, 0, 5, 0, 1, 1, 1
     ]).toString();
@@ -91,6 +131,43 @@ describe("Encode CENNZnut", () => {
                 0, 192, 128, 16, 246, 0, 0, 0, 0, 0, 0, 0, 128, 16, 178, 128, 0,
                 0, 0, 0, 0, 0, 0, 224, 116, 101, 115, 116, 105, 110, 103, 5, 0,
                 0, 1, 0, 5, 0, 1, 1, 1,
+              ]
+            }
+          }
+        }
+      }
+    }).toString();
+    expect(encodedLive).toEqual(encodedTest);
+  });
+
+  it("encodes 2 modules with constraints", () => {
+    const encodedTest = new Uint8Array([
+      128, 64, 103, 101, 110, 101, 114, 105, 99, 45, 97, 115, 115, 101, 116, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 64, 116, 114, 97,
+      110, 115, 102, 101, 114, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 240, 0, 192, 128, 16, 246, 0, 0, 0, 0, 0, 0, 0,
+      128, 16, 178, 128, 64, 97, 116, 116, 101, 115, 116, 97, 116, 105, 111,
+      110, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 64,
+      99, 114, 101, 97, 116, 101, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 112, 0, 0, 0, 0, 0, 0, 224, 116, 101,
+      115, 116, 105, 110, 103, 5,
+    ]).toString();
+    const encodedLive = cennznut.encode({
+      "modules": {
+        "generic-asset": {
+          "methods": {
+            "transfer": {
+              "constraints": [
+                0, 192, 128, 16, 246, 0, 0, 0, 0, 0, 0, 0, 128, 16, 178, 128,
+              ]
+            }
+          }
+        },
+        "attestation": {
+          "methods": {
+            "create": {
+              "constraints": [
+                0, 0, 0, 0, 0, 0, 224, 116, 101, 115, 116, 105, 110, 103, 5,
               ]
             }
           }
@@ -113,7 +190,29 @@ describe("Decode CENNZnut", () => {
       let c = cennznut.decode(encoded);
       expect(Object.keys(c.modules).length).toEqual(2);
   });
-  
+
+  it("decodes 0 constraint length byte as 1", () => {
+      const encoded = new Uint8Array([0,64,109,111,100,117,108,101,95,116,101,115,116,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,64,109,101,116,104,111,100,95,116,101,115,116,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]);
+      const c = cennznut.decode(encoded);
+      const l = c
+        .modules["module_test"]
+        .methods["method_test"]
+        .constraints
+        .length;
+      expect(l).toEqual(1);
+  });
+
+  it("decodes 1 constraint length byte as 2", () => {
+      const encoded = new Uint8Array([0,64,109,111,100,117,108,101,95,116,101,115,116,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,64,109,101,116,104,111,100,95,116,101,115,116,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,128,1,2]);
+      let c = cennznut.decode(encoded);
+      const l = c
+        .modules["module_test"]
+        .methods["method_test"]
+        .constraints
+        .length;
+      expect(l).toEqual(2);
+  });
+
   it("decodes with constraints", () => {
     const encoded = new Uint8Array([0,64,109,111,100,117,108,101,95,116,101,115,116,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,64,109,101,116,104,111,100,95,116,101,115,116,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,148,0,192,128,16,246,0,0,0,0,0,0,0,128,16,178,128,0,0,0,0,0,0,0,224,116,101,115,116,105,110,103,5,0,0,1,0,5,0,1,1,1]);
     let rawNut = cennznut.decode(encoded);
